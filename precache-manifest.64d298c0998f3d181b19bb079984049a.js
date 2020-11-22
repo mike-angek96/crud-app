b29e15a8f1c8970e6f79c3cea13f8643fa87d6c5
miki <angelarkangel91@hotmail.com>
2020-11-21 18:51:26 -0600

    Updates

---
+++ precache-manifest.64d298c0998f3d181b19bb079984049a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cc1d2739c580fb262ace747f090b8c4",
+    "url": "/crud-app/index.html"
+  },
+  {
+    "revision": "5c8f3020d0a00480ff5e",
+    "url": "/crud-app/static/css/main.49b31ff2.chunk.css"
+  },
+  {
+    "revision": "631f0899576887763e29",
+    "url": "/crud-app/static/js/2.b7201942.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/crud-app/static/js/2.b7201942.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c8f3020d0a00480ff5e",
+    "url": "/crud-app/static/js/main.63fbfb21.chunk.js"
+  },
+  {
+    "revision": "9199bf3a41bce913e545",
+    "url": "/crud-app/static/js/runtime-main.3dacf9d8.js"
+  }
+]);
